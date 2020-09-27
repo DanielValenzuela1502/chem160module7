fbafa0931bfef18a04c904de9d580870d1bc89d2
DanielValenzuela1502 <70194636+DanielValenzuela1502@users.noreply.github.com>
2020-09-26 20:29:57 -0700

    Add files via upload

---
+++ RNA.py
@@ -0,0 +1,11 @@
+seqs=["UUCGCCGACUGA","AUGCCUUGA","AUGCGGUGAUAA"]
+for seq in seqs:
+    print(seq)
+    if seq.startswith("AUG"):
+        print("Has start codon")
+    if seq.count("UGA")>=0 and not seq.endswith("UGA"):
+        print("Has selenocysteine")
+    elif seq.count("UGA")>0:
+        print("Has selenocysteine")
+        
+    
